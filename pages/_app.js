27d0b26e0575519b5e29c41dffd96c6d939dcaa0
shivansh singh <shivanshsingh9876@gmail.com>
2025-02-19 23:21:22 +0530

    fe

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import { AuthProvider } from "../context/AuthContext";
+import Navbar from "@/components/Navbar";
+ import Jobs from "@/components/Jobs";
+import LandingPage from "@/components/LandingPage";
+import Internships from "@/components/Internships";
+import Mentorship from "@/components/Mentorship";
+import Profile from "@/components/Profile";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+         <Navbar/>     
+      <LandingPage/>
+     <Jobs/>
+     <Internships/>
+     <Mentorship/>
+     <Profile/>
     </AuthProvider>
+
   );
 }
 
